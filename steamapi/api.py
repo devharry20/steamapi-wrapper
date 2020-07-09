dc65c39224b57db76a27cfdefa955e9f2b4f0360
Harry <42916213+devharry20@users.noreply.github.com>
2020-07-09 20:35:53 +0100

    removed helper functions and moved to helpers.py

--- steamapi/api.py
+++ steamapi/api.py
@@ -1,12 +1,4 @@
-import requests
-
-
-def _apicall(url):
-    r = requests.get(url)
-    return r.json()
-
-def str_format(*steamids):
-    return ', '.join([str(steamid) for steamid in steamids])
+from steamapi.helpers import _apicall, str_format
 
 
 class ISteamUser:
